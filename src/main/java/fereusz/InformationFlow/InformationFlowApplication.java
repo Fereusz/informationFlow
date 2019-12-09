68e38ec8186b4b919b73a4a206168fad93264474
Marcin Fereniec <marcin.fereniec@gmail.com>
2019-12-09 14:54:44 +0100

    Initial commit

---
+++ src/main/java/fereusz/InformationFlow/InformationFlowApplication.java
@@ -0,0 +1,13 @@
+package fereusz.InformationFlow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InformationFlowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InformationFlowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fereusz/InformationFlow/InformationFlowApplicationTests.java
@@ -0,0 +1,13 @@
+package fereusz.InformationFlow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InformationFlowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
