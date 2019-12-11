cfe9633610203dd322415c30750d909d0a1fcc57
Marcin Fereniec <marcin.fereniec@gmail.com>
2019-12-11 12:40:02 +0100

    PM add form

--- src/main/java/fereusz/InformationFlow/domain/repositories/FundRepository.java
+++ src/main/java/fereusz/InformationFlow/domain/repositories/FundRepository.java
@@ -3,6 +3,9 @@ package fereusz.InformationFlow.domain.repositories;
 import fereusz.InformationFlow.domain.entities.Fund;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface FundRepository extends JpaRepository<Fund,Long> {
+
 
 }
