58f7fac6f74bed38b3d02397b4e843df112c11df
Marcin Fereniec <marcin.fereniec@gmail.com>
2019-12-12 15:00:45 +0100

    CRUD completed

--- src/main/java/fereusz/InformationFlow/services/FundManagerCreateService.java
+++ src/main/java/fereusz/InformationFlow/services/FundManagerCreateService.java
@@ -1,8 +1,18 @@
 package fereusz.InformationFlow.services;
 
+import fereusz.InformationFlow.domain.entities.FundManager;
 import fereusz.InformationFlow.dtos.FundManagerDTO;
+import fereusz.InformationFlow.dtos.ManagerEditDTO;
+
+import java.util.List;
 
 public interface FundManagerCreateService {
 
     void create (FundManagerDTO fundManagerDTO);
+
+    List<FundManager> findAll();
+    ManagerEditDTO prepareUpdate (Long id);
+    void save(ManagerEditDTO managerEditDTO);
+    void delete(Long id);
+
 }
--- src/main/java/fereusz/InformationFlow/services/impl/DefaultFundManagerCreateService.java
+++ src/main/java/fereusz/InformationFlow/services/impl/DefaultFundManagerCreateService.java
@@ -1,15 +1,23 @@
 package fereusz.InformationFlow.services.impl;
 
+import fereusz.InformationFlow.domain.entities.Fund;
 import fereusz.InformationFlow.domain.entities.FundManager;
 import fereusz.InformationFlow.domain.entities.User;
 import fereusz.InformationFlow.domain.repositories.FundManagerRepository;
 import fereusz.InformationFlow.domain.repositories.UserRepository;
+import fereusz.InformationFlow.dtos.FundEditDTO;
 import fereusz.InformationFlow.dtos.FundManagerDTO;
+import fereusz.InformationFlow.dtos.ManagerEditDTO;
 import fereusz.InformationFlow.services.FundManagerCreateService;
+import org.apache.catalina.Manager;
 import org.modelmapper.ModelMapper;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.ui.ModelMap;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 @Transactional
@@ -33,4 +41,34 @@ public class DefaultFundManagerCreateService implements FundManagerCreateService
 
 
     }
+
+    @Override
+    public List<FundManager> findAll() {
+       return fundManagerRepository.findAll();
+    }
+
+    @Override
+    public ManagerEditDTO prepareUpdate(Long id) {
+        Optional<FundManager> manager = fundManagerRepository.findById(id);
+        ModelMapper mapper = new ModelMapper();
+        ManagerEditDTO managerToEdit = mapper.map(manager, ManagerEditDTO.class);
+        return managerToEdit;
+    }
+
+    @Override
+    public void save(ManagerEditDTO managerEditDTO) {
+        ModelMapper mapper = new ModelMapper();
+        FundManager manager = mapper.map(managerEditDTO, FundManager.class);
+        fundManagerRepository.save(manager);
+    }
+
+
+
+    @Override
+    public void delete(Long id) {
+        fundManagerRepository.deleteById(id);
+
+    }
+
+
 }
